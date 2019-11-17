bad5aefa8a9c0207f3a75e01c553d55645c69c75
katongole <katsisaac50@gmail.com>
2019-11-17 14:41:06 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -6,31 +6,52 @@ import jobsearchicon from './assets/jobsearch.svg';
 import WorkPermitsicon from './assets/WorkPermits.png';
 import vehicleTransferIcon from './assets/vehicleTransfer.png';
 import DrivingLicenceIcon from './assets/DrivingLicence1.png';
+import goodtimesIcon from './assets/goodtimesIcon.png';
 
 function App() {
   return (
-    <div className="grid-container">
+    <div className="wrapper">
+      <div class="container box1">
+        <header>
+          <NavBar/>
+          {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <polygon fill="white" points="0,100 100,0 100,100"/>
+            <polygon fill="white" points="0,100 -40,0 80,100"/>
+          </svg>}
+          <div style={{padding: '20px'}}>
+          <h1 className="text-heading">CONSULTING SERVICE FOR ALL</h1>
+          <h3>Our Mission</h3>
+          <p>we are here to serve you</p>
+          </div>
+          
+        </header>
+      </div>
     
-    <header>
-    <NavBar/>
-    {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-      <polygon fill="white" points="0,100 100,0 100,100"/>
-      <polygon fill="white" points="0,100 -40,0 80,100"/>
-    </svg>}
-
-    <h2>CONSULTING SERVICE FOR ALL</h2>
-    <h3>Kind of bussinesses</h3>
-    <p>Text before SVG. I'm some boring text. Such incredibly boring text. Who would possibly want to read such boring text?</p>
-  </header>
-  <h1>Our Services</h1>
-  <section className="container">
-  <div class="item-a"><img src={passporticon} width='50%' alt="pass"></img> <br/><span>Pass Port</span></div>
-  <div class="item-b"><img src={jobsearchicon} width='50%' alt="pass"></img> <br/>Job Search</div>
-  <div class="item-c"><img src={WorkPermitsicon} width='50%' alt="pass"></img> <br/>Work Permits</div>
-  <div class="item-d"><img src={vehicleTransferIcon} width='50%' alt="pass"></img> <br/>Vehicle Transfer</div>
-  <div class="item-e"><img src={DrivingLicenceIcon} width='50%' alt="pass"></img> <br/>Driving Licence</div>
-    
-  </section>
+    <div className="container box2">
+          <div style={{width: '115%'}}/* className="section-heading" */>
+            <h1>Our Services</h1> <br/>
+          </div>
+          <div class="item-a">
+            <img src={passporticon} width='50%' alt="pass"></img> <br/>
+            <span>Pass Port</span>
+          </div>
+          <div class="item-b">
+            <img src={jobsearchicon} width='50%' alt="pass"></img> <br/>
+            <span>Job Search</span>
+          </div>
+          <div class="item-c">
+            <img src={WorkPermitsicon} width='50%' alt="pass"></img> <br/>
+            <span>Work Permits</span>
+          </div>
+          <div class="item-d">
+            <img src={vehicleTransferIcon} width='50%' alt="pass"></img> <br/>
+            <span>Vehicle Transfer</span>
+          </div>
+          <div class="item-e">
+            <img src={DrivingLicenceIcon} width='50%' alt="pass"></img> <br/>
+            <span>Driving Licence</span>
+          </div>
+    </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React, { Component } from "react";
-import goodtimesIcon from '../assets/goodtimesIcon.png';
+import goodtimesIcon from '../assets/goodtimesIcon.ico';
  
 class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
         <nav className='navbar'>
-        <img src={goodtimesIcon} width="10%" height="10%" alt='icon'/>
-        <a href="#"><i class="fa fa-fw fa-user"></i> Login </a>
+
+        <div className="Icon"><img src={goodtimesIcon} alt='icon' width="auto" align="middle"/> <span className="IconText">Good Times Solutions</span></div>
+
+         {/* <div className="Icon"><img src={goodtimesIcon} width="auto" alt='icon'/>
+         <span className="IconText">Good Times Solutions</span></div> */}
+         <div className="nav-links">
+         <a href="#"><i class="fa fa-fw fa-user"></i> Login </a>
         <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact </a>
         <a href="#"><i class="fa fa-fw fa-search"></i> Search </a>
         <a class="active" href="#"> <i class="fa fa-fw fa-home"></i> Home </a>
-      
+        
+         </div>
+        
         </nav>
       </React.Fragment>
     );
