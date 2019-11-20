6cc86f70e952f85c9c98afbc363b0ff6f64a078a
katongole <katsisaac50@gmail.com>
2019-11-20 05:08:02 +0100

    Updates

---
+++ precache-manifest.e1632e682db383ae2057b328a9583705.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1878029895084be38ddddbd34818197e",
+    "url": "/goodtimessolutions/index.html"
+  },
+  {
+    "revision": "43454c296352a2aab229",
+    "url": "/goodtimessolutions/static/css/main.9c22a28e.chunk.css"
+  },
+  {
+    "revision": "505edf241498c181480a",
+    "url": "/goodtimessolutions/static/js/2.b9c53bd8.chunk.js"
+  },
+  {
+    "revision": "43454c296352a2aab229",
+    "url": "/goodtimessolutions/static/js/main.1c78f756.chunk.js"
+  },
+  {
+    "revision": "c1d88a1b57fb7199404a",
+    "url": "/goodtimessolutions/static/js/runtime~main.2caaa352.js"
+  },
+  {
+    "revision": "3ceda3498dd7f7a725c9248a65004c37",
+    "url": "/goodtimessolutions/static/media/DrivingLicence1.3ceda349.png"
+  },
+  {
+    "revision": "aaca79a83421fe75bb17857ec818ef94",
+    "url": "/goodtimessolutions/static/media/WorkPermits.aaca79a8.png"
+  },
+  {
+    "revision": "49d43404cdfa312f7c0ce31ac53729c6",
+    "url": "/goodtimessolutions/static/media/company-formation.49d43404.png"
+  },
+  {
+    "revision": "3585f758dd1f9f2b9fa95345d4c98f0c",
+    "url": "/goodtimessolutions/static/media/email-icon.3585f758.png"
+  },
+  {
+    "revision": "a4025a98ed2343cdaee51ceea61432d3",
+    "url": "/goodtimessolutions/static/media/goodtimesIcon.a4025a98.ico"
+  },
+  {
+    "revision": "eefb964cfd20173f58a259caf7a9a41a",
+    "url": "/goodtimessolutions/static/media/jobsearch.eefb964c.svg"
+  },
+  {
+    "revision": "46a51865a830894c2988715bddc558bd",
+    "url": "/goodtimessolutions/static/media/passport-512.46a51865.png"
+  },
+  {
+    "revision": "7417b83d402be7b5fc19667a1dfdaf5d",
+    "url": "/goodtimessolutions/static/media/phone-icon.7417b83d.png"
+  },
+  {
+    "revision": "8fbb74d8dd97913fc0cd517b77c48d82",
+    "url": "/goodtimessolutions/static/media/vehicleTransfer.8fbb74d8.png"
+  }
+]);
